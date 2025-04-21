b331319fbb7b4c2a15fce5adc3d52d8f74772354
Corvin Schapoehler <co.schapoehler@gmail.com>
2025-04-21 10:00:07 +0200

    Update __init__.py

--- custom_components/donetick_custom/__init__.py
+++ custom_components/donetick_custom/__init__.py
@@ -3,5 +3,5 @@ from homeassistant import core
 
 async def async_setup(hass: core.HomeAssistant, config: dict) -> bool:
     """Set up the Donetick Custom component."""
-    hass.states.async_set("donetick.a", "test")
+    hass.states.async_set("donetick.test", "test")
     return True
